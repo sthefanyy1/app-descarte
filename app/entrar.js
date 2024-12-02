9e655b0a9a6df2df100e06d2ce8237bbab3e9494
sthefany <sgposl1@aluno.ifal.edu.br>
2024-12-02 15:37:46 -0300

    Tela de início, de entrada, de cadastro e esqueceu senha

--- app/entrar.js
+++ app/entrar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { View, StyleSheet, Text } from 'react-native';
 import { Appbar, TextInput, Button } from 'react-native-paper';
 import { Link, router } from 'expo-router';
@@ -11,6 +12,7 @@ const Entrar = () => {
     return (
         <View style={styles.container}>
             <View>
+            <StatusBar style="auto" />
                 <Appbar.Header style={styles.header}>
                     <Appbar.BackAction onPress={() => { router.back() }} color="#4CA04A" />
                 </Appbar.Header>
