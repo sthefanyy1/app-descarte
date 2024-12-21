65e18fa4461d78bcc89656671fa134ede8dec23f
manobr1975 <mbal1@aluno.ifal.edu.br>
2024-12-21 08:36:29 -0300

    Firebase no firebaseConfig.js

--- firebase.config.js
+++ firebase.config.js
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,4 +24,7 @@ const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
 
-export default auth;
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export default { auth, db };
