477ecfcdc30b2c78548bf747d034bbeab170c653
sthefany <sgposl1@aluno.ifal.edu.br>
2025-01-13 14:24:32 -0300

    Tela "escolher" municípios e Tela de mais detalhes

--- app/pontos/[id].js
+++ app/pontos/[id].js
@@ -1,8 +1,10 @@
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ActivityIndicator, StyleSheet, Pressable, Image } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { Appbar, Avatar } from 'react-native-paper';
+import { StatusBar } from 'expo-status-bar';
+import { router, useLocalSearchParams } from 'expo-router';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../firebase.config';
+import { auth, db } from '../../firebase.config';
 
 const Id = () => {
     const { id } = useLocalSearchParams();
@@ -30,18 +32,67 @@ const Id = () => {
     }, []);
 
     return (
-        <View>
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+
+                 <Appbar.Header style={styles.header}>
+                    <Appbar.BackAction onPress={() => { router.back() }} color="#4CA04A" />
+                </Appbar.Header> 
+
+                {/* Contêiner para logo e avatar */}
+                <View style={styles.header}>
+                    <Image source={require('../../assets/logo.jpeg')} style={styles.logotipo} /> 
+                    <Pressable onPress={() => router.navigate('/perfil')}>
+                        <Avatar.Text size={50} label={auth.currentUser.displayName.charAt(0)} />
+                    </Pressable>
+                </View>
+
             {loading ? (
                 <ActivityIndicator />
             ) : (
                 <>
-                    <Text>{pontos.nome}{'\n'}{'\n'}</Text>
-                    <Text>Endereço: {pontos.endereco}{'\n'}{'\n'}</Text>
-                    <Text>Contato: {pontos.telefone}</Text>
+                    <Text style={styles.texto}>{pontos.nome}{'\n'}{'\n'}</Text>
+                    <Text style={styles.endereco}>Endereço: {pontos.endereco}{'\n'}{'\n'}</Text>
+                    <Text style={styles.contato}>Contato: {pontos.telefone}</Text>
                 </>
             )}
         </View>
     )
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        justifyContent: 'flex-start',
+    },
+    header: {
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    logotipo: {
+        width: 150,
+        height: 150,
+    },
+    texto: {
+        marginLeft: 40,
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    endereco: {
+        marginLeft: 20,
+        fontSize: 16,
+    },
+    contato: {
+        marginLeft: 20,
+        fontSize: 16,
+    },
+});
 
 export default Id;
