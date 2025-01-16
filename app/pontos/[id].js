fa8ffe0375ed9f1226486b651940e1f6d56a34da
sthefany <sgposl1@aluno.ifal.edu.br>
2025-01-16 14:09:13 -0300

    Mapa não está pegando sem o delta

--- app/pontos/[id].js
+++ app/pontos/[id].js
@@ -41,7 +41,14 @@ const Id = () => {
                 </Appbar.Header> 
 
                 <View style={styles.container}>
-                    <MapView style={styles.map} />
+                    <MapView style={styles.map} 
+                        initialRegion={{
+                            latitude: 37.78825,
+                            longitude: -122.4324,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                    />
                 </View>
 
             {loading ? (
