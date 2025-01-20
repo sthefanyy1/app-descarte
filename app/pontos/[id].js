f7f6223ed9e1361b7605f9bec8411ca934eb22db
sthefany <sgposl1@aluno.ifal.edu.br>
2025-01-20 16:25:42 -0300

    Ajuste da latitude e longitude dos pontos de coleta

--- app/pontos/[id].js
+++ app/pontos/[id].js
@@ -1,7 +1,132 @@
+// import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+// import React, { useEffect, useState } from 'react';
+// import { Appbar } from 'react-native-paper';
+// import MapView from 'react-native-maps';
+// import {Marker} from 'react-native-maps';
+// import { StatusBar } from 'expo-status-bar';
+// import { router, useLocalSearchParams } from 'expo-router';
+// import { doc, getDoc } from 'firebase/firestore';
+// import { db } from '../../firebase.config';
+
+// const Id = () => {
+//     const { id } = useLocalSearchParams();
+//     const [loading, setLoading] = useState(true);
+//     const [ponto, setPonto] = useState({});
+   
+//     const getPonto = async () => {
+//         try {
+//             //console.log('1');
+//             const docRef = doc(db, "pontos", id);
+//             //console.log('2');
+//             const docSnap = await getDoc(docRef);
+//             //console.log('3');
+
+//             if (docSnap.exists()) {
+//                 setPonto(docSnap.data());
+//                 //console.log(ponto);
+//             } else {
+//                 console.log("Erro");
+//             }
+
+//         } catch (error) {
+//             console.error(error);
+//         } finally {
+//             setLoading(false);
+//         }
+//     };
+
+//     useEffect(() => {
+//         getPonto();
+//     }, []);
+
+//     return (
+//         <View style={styles.container}>
+//             <StatusBar style="auto" />
+
+//                 <Appbar.Header style={styles.header}>
+//                     <Appbar.BackAction onPress={() => { router.back() }} color="#346E33" />
+//                 </Appbar.Header> 
+
+//             {loading ? (
+//                 <ActivityIndicator />
+//             ) : (
+//                 <>
+
+//             <View style={styles.container}>
+//                 <MapView style={styles.map} 
+//                     initialRegion={{
+//                         latitude: ponto.coordenadas.latitude,
+//                         longitude: ponto.coordenadas.longitude,
+//                         latitudeDelta: 0.0922,
+//                         longitudeDelta: 0.0421,
+//                     }}
+//                 />
+//                 <Marker
+//                     //key={index}
+//                     coordinate={ponto.latlng}
+//                     //title={ponto.title}
+//                     //description={ponto.description}
+//                 />
+//             </View>
+//                     <Text style={styles.texto}>{ponto.nome}{'\n'}{'\n'}</Text>
+//                     <Text style={styles.endereco}>Endereço: {ponto.endereco}{'\n'}{'\n'}</Text>
+//                     <Text style={styles.aberto}>Aberto de domingo a domingo das 6h até às 18h{'\n'}{'\n'}</Text>
+//                     <Text style={styles.contato}>Contato: {ponto.telefone}</Text>
+//                 </>
+
+//             )}
+//         </View>
+//     )
+// };
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         padding: 10,
+//         marginLeft: 10,
+//         marginRight: 10,
+//         justifyContent: 'flex-start',
+//     },
+//     header: {
+//         backgroundColor: '#fff',
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//         alignItems: 'center',
+//         marginBottom: 20,
+//     },
+//     texto: {
+//         marginLeft: 40,
+//         fontSize: 20,
+//         fontWeight: 'bold',
+//     },
+//     endereco: {
+//         marginLeft: 20,
+//         fontSize: 16,
+//     },
+//     aberto: {
+//         marginLeft: 20,
+//         fontSize: 16,
+//         fontWeight: 'bold',
+//         color: '#346E33',
+//     },
+//     contato: {
+//         marginLeft: 20,
+//         fontSize: 16,
+//     },
+//     map: {
+//         width: '100%',
+//         height: '100%',
+//     },
+// });
+
+// export default Id;
+
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { Appbar } from 'react-native-paper';
 import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
 import { router, useLocalSearchParams } from 'expo-router';
 import { doc, getDoc } from 'firebase/firestore';
@@ -14,15 +139,11 @@ const Id = () => {
    
     const getPonto = async () => {
         try {
-            //console.log('1');
             const docRef = doc(db, "pontos", id);
-            //console.log('2');
             const docSnap = await getDoc(docRef);
-            //console.log('3');
 
             if (docSnap.exists()) {
                 setPonto(docSnap.data());
-                //console.log(ponto);
             } else {
                 console.log("Erro");
             }
@@ -42,34 +163,41 @@ const Id = () => {
         <View style={styles.container}>
             <StatusBar style="auto" />
 
-                <Appbar.Header style={styles.header}>
-                    <Appbar.BackAction onPress={() => { router.back() }} color="#346E33" />
-                </Appbar.Header> 
+            <Appbar.Header style={styles.header}>
+                <Appbar.BackAction onPress={() => { router.back() }} color="#346E33" />
+            </Appbar.Header> 
 
             {loading ? (
                 <ActivityIndicator />
             ) : (
                 <>
-
-            <View style={styles.container}>
-                <MapView style={styles.map} 
-                    initialRegion={{
-                        latitude: ponto.coordenadas.latitude,
-                        longitude: ponto.coordenadas.longitude,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-                />
-            </View>
+                    <View style={styles.container}>
+                        <MapView style={styles.map} 
+                            initialRegion={{
+                                latitude: ponto.coordenadas.latitude,
+                                longitude: ponto.coordenadas.longitude,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            }}
+                        >
+                            <Marker
+                                coordinate={{
+                                    latitude: ponto.coordenadas.latitude,
+                                    longitude: ponto.coordenadas.longitude
+                                }}
+                                title={ponto.nome}
+                                description={ponto.endereco}
+                            />
+                        </MapView>
+                    </View>
                     <Text style={styles.texto}>{ponto.nome}{'\n'}{'\n'}</Text>
                     <Text style={styles.endereco}>Endereço: {ponto.endereco}{'\n'}{'\n'}</Text>
                     <Text style={styles.aberto}>Aberto de domingo a domingo das 6h até às 18h{'\n'}{'\n'}</Text>
                     <Text style={styles.contato}>Contato: {ponto.telefone}</Text>
                 </>
-
             )}
         </View>
-    )
+    );
 };
 
 const styles = StyleSheet.create({
